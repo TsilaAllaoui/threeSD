22c8e1ebd02a5c49f443aef28b88aa365e7aefe0
TsilaAllaoui <tsila.allaoui@gmail.com>
2024-10-31 12:15:39 +0300

    Update build.sh

--- .ci/windows-msvc/build.sh
+++ .ci/windows-msvc/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 
 mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_TOOLCHAIN_FILE="$(pwd)/../CMakeModules/MSVCCache.cmake" -DUSE_CCACHE=ON -DWARNINGS_AS_ERRORS=OFF -DUSE_BUNDLED_QT=1
+cmake .. -DCMAKE_BUILD_TYPE=Release -G Ninja -DCMAKE_TOOLCHAIN_FILE="$(pwd)/../CMakeModules/MSVCCache.cmake" -DUSE_CCACHE=ON -DWARNINGS_AS_ERRORS=OFF -DUSE_BUNDLED_QT=0
 
 ninja
 # show the caching efficiency
